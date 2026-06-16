670ac91213cb2b46c5f8810f8d30d3c2869d622f
Ben Cope <jbc121707@gmail.com>
2026-06-16 23:41:18 +0100

    Update neo-tree and treesitter plugin configurations for improved functionality

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,30 +1,28 @@
 -- ~/.config/nvim/lua/plugins/neo-tree.lua
 
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle file tree" })
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>e", ":Neotree focus<CR>", { desc = "Focus file tree" })
 
-        -- Inside neo-tree: <leader>cd changes working directory to the hovered folder
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = "neo-tree",
-            callback = function()
-                vim.keymap.set("n", "<leader>cd", function()
-                    local node = require("neo-tree.sources.manager")
-                        .get_state("filesystem")
-                        .tree:get_node()
-                    if node and node.type == "directory" then
-                        vim.cmd("cd " .. vim.fn.fnameescape(node.path))
-                        vim.notify("cd → " .. node.path, vim.log.levels.INFO)
-                    end
-                end, { buffer = true, desc = "Change directory to folder" })
-            end,
-        })
-    end,
+		-- Inside neo-tree: <leader>cd changes working directory to the hovered folder
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "neo-tree",
+			callback = function()
+				vim.keymap.set("n", "<leader>cd", function()
+					local node = require("neo-tree.sources.manager").get_state("filesystem").tree:get_node()
+					if node and node.type == "directory" then
+						vim.cmd("cd " .. vim.fn.fnameescape(node.path))
+						vim.notify("cd → " .. node.path, vim.log.levels.INFO)
+					end
+				end, { buffer = true, desc = "Change directory to folder" })
+			end,
+		})
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 -- ~/.config/nvim/lua/plugins/treesitter.lua
 
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master", -- stable old API; new "main" branch requires tree-sitter-cli >= 0.26.1
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "lua", "python", "typescript", "javascript" },
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main", -- stable old API; new "main" branch requires tree-sitter-cli >= 0.26.1
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "lua", "python", "typescript", "javascript" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
